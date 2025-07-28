3ea2adbbff2b659c37a88a868c310ade1ec08f1d
Ritul Dani <100018102+rituldani@users.noreply.github.com>
2025-07-28 20:41:07 +0530

    Update useGetAllUsers.jsx

--- Frontend/src/context/useGetAllUsers.jsx
+++ Frontend/src/context/useGetAllUsers.jsx
@@ -18,7 +18,7 @@ function useGetAllUsers() {
                         Authorization: `Bearer ${token}` // ✅ send token in headers
                     }
                 });
-                console.log("Sending token:", user.token);
+                console.log("Sending token:", token);
                 setAllUsers(response.data);
             } catch(error){
                 console.error("Error in getAllUsers:", error);
