ac35c43e820cba23d6b5cd2a53e6cef4a14b1a47
Ritul Dani <100018102+rituldani@users.noreply.github.com>
2025-07-28 19:03:54 +0530

    Update server.js

--- Backend/SocketIO/server.js
+++ Backend/SocketIO/server.js
@@ -6,7 +6,10 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: [
+          "http://localhost:5173",
+          "https://real-time-chat-app-git-main-ritul-danis-projects.vercel.app"
+        ],
         methods: ["GET", "POST"],
     }
 })
