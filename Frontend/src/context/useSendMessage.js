2f4499e5f98a8f544bf74c4d19d09cc0d99c3b6c
Ritul Dani <100018102+rituldani@users.noreply.github.com>
2025-07-28 18:04:29 +0530

    Update useSendMessage.js

--- Frontend/src/context/useSendMessage.js
+++ Frontend/src/context/useSendMessage.js
@@ -13,7 +13,7 @@ function useSendMessage() {
         // console.log(message)
         try {
             console.log(selectedConversation._id)
-            const res = await axios.post(`http://localhost:3000/message/send/${selectedConversation._id}`,
+            const res = await axios.post(`${import.meta.env.VITE_API_URL}/message/send/${selectedConversation._id}`,
                 {message},
                 {
                 headers: {
